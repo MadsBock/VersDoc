c4e05a985883cbc246e3c420328052152f639b20
Mads <bockchristensen@gmail.com>
2017-11-13 09:30:12 +0100

    Wrote the truth!

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Version Control!!!");
+            Console.WriteLine("AaB er de bedste!!!");
             Console.ReadKey();
         }
     }
