46620d1ecb94a86d3b4313c2a9cb30d5b61baced
Mads <bockchristensen@gmail.com>
2017-11-13 09:28:02 +0100

    Added Hello World code!

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Version Control!!!");
+            Console.ReadKey();
         }
     }
 }
