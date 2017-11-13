eb4aa990fc9bbf8e8264121c7a3e71e717343547
Fumlefinger <jann2273@gmail.com>
2017-11-13 11:17:53 +0100

    123

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("AaB er de bedste!!!");
+            Console.WriteLine("og det var dagens joke");
             Console.ReadKey();
         }
     }
